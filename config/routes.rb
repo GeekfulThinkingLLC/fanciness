26e8c71aed1bac4447d07ca40f938ddbcb146f8a
Trevor <trevorhoward2@hotmail.com>
2017-11-05 22:09:06 -0700

    Added side nav and a few other things

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get '/home', to: 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
   root 'static_pages#home'
 end
